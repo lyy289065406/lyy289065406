352c454cd1817b701fdc49cbfadf063c5e652831
EXP <289065406@qq.com>
2020-08-10 23:29:52 +0800

    test

--- main.py
+++ main.py
@@ -5,9 +5,74 @@
 # @File   : main.py
 # -----------------------------------------------
 
-import time
+from python_graphql_client import GraphqlClient
+import pathlib
+import os
+
+PRJ_ROOT = pathlib.Path(__file__).parent.resolve()
+client = GraphqlClient(endpoint="https://api.github.com/graphql")
+TOKEN = os.environ.get("GITHUB_TOKEN", "")
+
+def make_query(after_cursor=None):
+    return """
+query {
+    viewer {
+        login
+    }
+    repository(owner: "lyy289065406", name: "threat-broadcast") {
+        issues(first: 2, after:AFTER) {
+            id
+            title
+            body
+            pageInfo {
+                hasNextPage
+                endCursor
+            }
+        }
+    }
+}
+""".replace(
+        "AFTER", '"{}"'.format(after_cursor) if after_cursor else "null"
+    )
+
+def fetch_releases(oauth_token):
+    repos = []
+    releases = []
+    repo_names = set()
+    has_next_page = True
+    after_cursor = None
+
+    while has_next_page:
+        data = client.execute(
+            query=make_query(after_cursor),
+            headers={"Authorization": "Bearer {}".format(oauth_token)},
+        )
+        print(data)
+        print(json.dumps(data, indent=4))
+        print()
+        # for repo in data["data"]["viewer"]["repositories"]["nodes"]:
+        #     if repo["releases"]["totalCount"] and repo["name"] not in repo_names:
+        #         repos.append(repo)
+        #         repo_names.add(repo["name"])
+        #         releases.append(
+        #             {
+        #                 "repo": repo["name"],
+        #                 "repo_url": repo["url"],
+        #                 "description": repo["description"],
+        #                 "release": repo["releases"]["nodes"][0]["name"]
+        #                 .replace(repo["name"], "")
+        #                 .strip(),
+        #                 "published_at": repo["releases"]["nodes"][0]["publishedAt"],
+        #                 "published_day": repo["releases"]["nodes"][0][
+        #                     "publishedAt"
+        #                 ].split("T")[0],
+        #                 "url": repo["releases"]["nodes"][0]["url"],
+        #             }
+        #         )
+        has_next_page = data["data"]["repository"]["issues"]["pageInfo"]["hasNextPage"]
+        after_cursor = data["data"]["repository"]["issues"]["pageInfo"]["endCursor"]
+    return releases
 
 if __name__ == '__main__':
-    with open('./test.dat', 'a+') as file :
-        file.write(time.strftime("%Y-%m-%d %H:%M:%S\n", time.localtime()))
+    fetch_releases(TOKEN)
         
