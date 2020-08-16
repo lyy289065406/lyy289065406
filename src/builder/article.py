cef1393bc56f181129979704faa0b64c0e71eb7b
EXP <289065406@qq.com>
2020-08-16 20:51:38 +0800

    0816

--- src/builder/article.py
+++ src/builder/article.py
@@ -4,6 +4,12 @@
 # @Time   : 2020/8/11 22:17
 # -----------------------------------------------
 
+import requests
+
+
+SITEMAP_EXP_BLOG = 'https://exp-blog.com/gitbook/book/sitemap.xml'
+SITEMAP_RE0_WEB = 'https://lyy289065406.github.io/re0-web/gitbook/book/sitemap.xml'
+
 
 def build(repos) :
     pass
